a59329431e1192842f538c700fedae51f2682405
keychained <kihoonchoi91@gmail.com>
2020-11-11 03:09:22 -0500

    channels

---
+++ frontend/components/channel/channel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class ChannelMain extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+   }
+
+   render() {
+       return {
+           
+       }
+   }
+}
+
+export default ChannelMain;
--- frontend/components/channel/channel_container.jsx
+++ frontend/components/channel/channel_container.jsx
@@ -1,11 +1,15 @@
 import { connect } from "react-redux";
 import ChannelMain from "./channel";
+import { fetchChannels, fetchChannel } from "../../actions/channel_actions"
 
 const msp = state => ({
     errors: state.errors.channel,
-    servers: Object.values(state.entities.channels),
+    channels: Object.values(state.entities.channels),
 });
 
 const mdp = dispatch => ({
-    
+    getAllChannels: serverId => dispatch(fetchChannels(serverId)),
+    getChannel: channelId => dispatch(fetchChannel(channelId))
 })
+
+export default connect(msp, mdp)(ChannelMain);

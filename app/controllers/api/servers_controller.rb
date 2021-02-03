c3e259b858ebbbc41249ceeb4b9489eb79b35042
keychained <kihoonchoi91@gmail.com>
2021-02-03 17:07:38 -0500

    refactor

--- app/controllers/api/servers_controller.rb
+++ app/controllers/api/servers_controller.rb
@@ -18,10 +18,26 @@ class Api::ServersController < ApplicationController
         @server.user_id = current_user.id
 
         if @server.save
+            ServerMember.create({member_id: @server.user_id, server_id: @server.id})
             Channel.create({title: "general", server_id: @server.id})
             render :show
         else
             render json: @server.errors.full_messages, status: 422
+        end
+    end
+
+    def join
+        @server = Server.find_by(invite_code: params[:inviteCode])
+    
+        if @server
+            if current_user.servers.include?(@server)
+                render json: ["Already a part of this server!"], status: 422
+            else
+                ServerMember.create({member_id: current_user.id, server_id: @server.id})
+                render :show
+            end
+        else
+          render json: ["Bad code!"], status: 422
         end
     end
 
@@ -38,6 +54,6 @@ class Api::ServersController < ApplicationController
     private
 
     def server_params
-        params.require(:server).permit(:title, :user_id)
+        params.require(:server).permit(:title, :user_id, :invite_code)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_222252) do
+ActiveRecord::Schema.define(version: 2021_02_03_215221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_01_20_222252) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invite_code"
     t.index ["title"], name: "index_servers_on_title"
     t.index ["user_id"], name: "index_servers_on_user_id"
   end

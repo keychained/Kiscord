7627b31687bb52c261259f12e2c74685196d9559
keychained <kihoonchoi91@gmail.com>
2021-02-24 04:18:36 -0500

    action

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
+import actionCable from 'actioncable';
+
+const CableApp = {};
+
+CableApp.cable = actionCable.createConsumer(`ws://${windows.location.hostname}:3000/cable`)
 
 const Root = ({ store }) => (
     <Provider store={store}>
         <HashRouter>
-            <App />
+            <App cableApp={CableApp} />
         </HashRouter>
     </Provider>
 );

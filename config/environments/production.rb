1106b3e8f8d118b2089e80e67401596061d66465
keychained <kihoonchoi91@gmail.com>
2021-03-11 22:55:42 -0500

    action

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://localhost:3000/cable'
-  config.action_cable.allowed_request_origins = [ /http:\/\/localhost.*/ ]
+  # config.action_cable.url = 'wss://localhost:3000/cable'
+  config.action_cable.url = 'wss://redistogo-flat-27580.herokuapp.com/cable'
+  # config.action_cable.allowed_request_origins = [ /http:\/\/localhost.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://kiscord.herokuapp.com', /http:\/\/kiscord.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
